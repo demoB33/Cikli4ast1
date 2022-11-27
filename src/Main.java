62a5a15912dd754893069e462b74b8cc95364fa0
demoB33 <vasilenko-s@list.ru>
2022-11-27 17:39:56 +0700

    Задание3.1

--- src/Main.java
+++ src/Main.java
@@ -47,6 +47,15 @@ public class Main {
         for (int f = 1; f <= 512; f = f * 2){
             System.out.println("Число " + f);
         }
+        //Задание 3.1
+        System.out.println("Задание 3.3 :");
+        int salary = 29000;
+        int total =  0;
+        for (int i = 1; i <= 12; i++){
+            total = total + salary;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+
 
 
 
