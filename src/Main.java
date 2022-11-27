1317347d4773ea285162bce1e1ba6cb06e032cc1
demoB33 <vasilenko-s@list.ru>
2022-11-27 16:33:25 +0700

    Повторение из видио урока

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Повторение из видео");
+
+        for(int i = 0; i <= 10; i++){
+            System.out.println("итерация цикла  " + i);
+        }
+
+
+    }
+}
